b3661b5debc0a944b1886967b1a0201aac91e3ab
Alf Stenbrunn <alfs@axis.com>
2021-06-03 23:17:34 +0200

    Add documentation and scripts
    Change-Id: Ica0d17695824a8d1718986da046da45cdbcdb3d9

--- playwright_test/test.js
+++ playwright_test/test.js
@@ -1,12 +1,11 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  //let browser = await chromium.launch({headless: false, slowMo: 100});
+  /*  comment the row below and de-comment the ones below
+      to run the test using an existing browser context
+  */
+  let browser = await chromium.launch({headless: false, slowMo: 100});
 
-   let browser = await chromium.connectOverCDP({
-     endpointURL: 'http://localhost:9222',
-     headless: false,
-   });
   const context = await browser.newContext({
     viewport: {
       width: 1280, // 1280 in commit 1ee6578
@@ -14,9 +13,29 @@ const { chromium } = require('playwright');
     }
   })
   const page = await context.newPage();
-  //const page = await browser.newPage();
   await page.goto('http://localhost:3000');
   await page.hover('button');
   await page.click('[data-testid=facebook]')
   browser.close()
+
+  // needs a running browser to work
+  // use --remote-debugging-port=9222
+  // if using --headless the test will pass, otherwise not
+
+  let browserCDP = await chromium.connectOverCDP({
+    endpointURL: 'http://localhost:9222',
+    headless: false,
+    slowMo: 100
+  });
+  const contextCDP = await browserCDP.newContext({
+    viewport: {
+      width: 1280, // 1280 in commit 1ee6578
+      height: 720, // 720 in commit 1ee6578
+    }
+  })
+  const pageCDP = await contextCDP.newPage();
+  await pageCDP.goto('http://localhost:3000');
+  await pageCDP.hover('button');
+  await pageCDP.click('[data-testid=facebook]')
+  browserCDP.close()
 })();
